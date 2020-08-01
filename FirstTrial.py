fff611205b6e7475d581fa381c9a003afa5255b8
Furyton <1543159426@qq.com>
2020-08-01 17:27:20 +0800

    master

--- FirstTrial.py
+++ FirstTrial.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from keras.datasets import mnist
 
-print("hello world")
-
-
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
