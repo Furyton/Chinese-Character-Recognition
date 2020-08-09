dbd4eea15faa043399b23726f5d82b0c05cb2250
Furyton <1543159426@qq.com>
2020-08-09 18:32:20 +0800

    自己随便搭建了一个网络,能够训练很小规模的数据,但是效果貌似不太好,可能是数据太少

--- DataGenerator.py
+++ DataGenerator.py
@@ -2,8 +2,10 @@ import json
 import os
 import shutil
 import numpy as np
+import matplotlib.pyplot as plt
 
-# from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
 
 class_list_dir = "C:/Users/Furyton/Documents/Machine Learning/Original_dataset/CASIA-Classes.txt"
 original_data_dir = "C:/Users/Furyton/Documents/Machine Learning/Original_dataset"
@@ -15,6 +17,7 @@ class DataGenerator:
         self.ch_num_dict = {} # a dict from character to its order
         self.num_ch_dict = {} # a dict from order to its charactor
 
+        self.cur_charset = None
     # load file "CASIA-Classes.txt"
     def load_class_list(self):
         content = None
@@ -29,10 +32,21 @@ class DataGenerator:
         self.num_ch_dict = {val:key for key, val in self.ch_num_dict.items()}
     
     # 从总的数据集中选出一个较小的训练集, 从训练集分出验证集, 测试集保持不变
-    def pick_small_dataset(self, char_number = 3755,  picked_number = 240, validation_rate = 0.2):
+    def pick_small_dataset(self, char_number = 3755,  picked_number = 240, validation_rate = 0.2, ignoreExistence = False):
+        # char_number 想要训练的汉字种类大小
+        # picked_number 每个种类的汉字选出样本图片的数目
+        # validation_rate 验证集占训练集的比例
+        # ignoreExistence 当已经存在  base_data_dir 时是否继续(若存在则删除)
         if self.ch_num_dict is None:
             self.load_class_list()
         
+        if os.path.exists(base_data_dir):
+            if not ignoreExistence:
+                print("文件夹已存在,操作取消")
+                return
+            else:
+                shutil.rmtree(base_data_dir)
+
         os.mkdir(base_data_dir)
 
         train_dir = os.path.join(base_data_dir, 'train')
@@ -51,15 +65,15 @@ class DataGenerator:
         train_count = picked_number - validation_count
         
         ClassFileList = [name for name in os.listdir(original_train)if os.path.isdir(os.path.join(original_train, name))][:char_number]
-        
+        self.cur_charset = ClassFileList
+
         for fileName in ClassFileList:
             org_path = os.path.join(original_train, fileName)
 
-            order = self.ch_num_dict[fileName]
-            new_train = os.path.join(train_dir, str(order))
+            new_train = os.path.join(train_dir, fileName)
             os.mkdir(new_train)
 
-            new_vali = os.path.join(validation_dir, str(order))
+            new_vali = os.path.join(validation_dir, fileName)
             os.mkdir(new_vali)
 
             PNGFileList = os.listdir(org_path)
@@ -70,13 +84,13 @@ class DataGenerator:
 
             for pngName in PNGFileList[:train_count]:
                 src = os.path.join(org_path, pngName)
-                dst = os.path.join(new_train, str(order) + '.' + str(cnt) + '.png')
+                dst = os.path.join(new_train, fileName + '.' + str(cnt) + '.png')
                 shutil.copyfile(src, dst)
 
                 cnt += 1
             for pngName in PNGFileList[train_count:picked_number]:
                 src = os.path.join(org_path, pngName)
-                dst = os.path.join(new_vali, str(order) + '.' + str(cnt) + '.png')
+                dst = os.path.join(new_vali, fileName + '.' + str(cnt) + '.png')
                 shutil.copyfile(src, dst)
 
                 cnt += 1
@@ -88,8 +102,7 @@ class DataGenerator:
         for fileName in ClassFileList:
             org_path = os.path.join(original_test, fileName)
 
-            order = self.ch_num_dict[fileName]
-            new_test = os.path.join(test_dir, str(order))
+            new_test = os.path.join(test_dir, fileName)
             os.mkdir(new_test)
 
             PNGFileList = os.listdir(org_path)
@@ -98,12 +111,69 @@ class DataGenerator:
 
             for pngName in PNGFileList:
                 src = os.path.join(org_path, pngName)
-                dst = os.path.join(new_test, str(order) + '.' + str(cnt) + '.png')
+                dst = os.path.join(new_test, fileName + '.' + str(cnt) + '.png')
                 shutil.copyfile(src, dst)
 
                 cnt += 1
 
-test = DataGenerator()
+    def data_gen(self, base_dir = base_data_dir, batchSize = 512, targetSize = (64, 64)):
+        if not os.path.exists(base_dir):
+            print("数据集所在目录不存在")
+            return
+        
+        train_dir = os.path.join(base_data_dir, 'train')
+        
+        validation_dir = os.path.join(base_data_dir, 'validation')
 
-test.load_class_list()
-# test.pick_small_dataset(char_number=2, picked_number=2, validation_rate=0.5)
+        test_dir = os.path.join(base_data_dir, 'test')
+
+
+        train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2
+        )
+        test_datagen = ImageDataGenerator(rescale=1./255)
+
+        train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=targetSize,
+            batch_size=batchSize,
+            class_mode='categorical',
+            color_mode='grayscale',
+            classes=self.cur_charset
+        )
+        validation_generator = test_datagen.flow_from_directory(
+            validation_dir,
+            target_size=targetSize,
+            batch_size=batchSize,
+            class_mode='categorical',
+            color_mode='grayscale',
+            classes=self.cur_charset
+        )
+        test_generator = test_datagen.flow_from_directory(
+            test_dir,
+            target_size=targetSize,
+            batch_size=batchSize,
+            class_mode='categorical',
+            color_mode='grayscale',
+            classes=self.cur_charset
+        )
+
+        return train_generator, validation_generator, test_generator
+
+# test = DataGenerator()
+
+# test.load_class_list()
+# test.pick_small_dataset(char_number=20, picked_number=10, validation_rate=0, ignoreExistence=False)
+
+# gen = test.data_gen()
+
+# for data_batch, label_batch in gen:
+#     plt.imshow(image.array_to_img(data_batch[0]))
+#     print(label_batch[0])
+#     break
+# plt.show()
---
+++ MyModel.py
@@ -0,0 +1,75 @@
+import DataGenerator
+from keras import layers
+from keras import models
+from keras import optimizers
+import matplotlib.pyplot as plt
+
+tot_char = 3755
+_steps_per_epoch = 52
+_epochs = 100
+_validation_steps = 20
+
+def train(model, img_size = (64, 64), charset_size=10, validationRate = 0.3, pickedNumber = 240):
+    dg = DataGenerator.DataGenerator()
+    dg.pick_small_dataset(char_number=charset_size, picked_number=pickedNumber, validation_rate=validationRate)
+    train_generator, validation_generator, test_generator = dg.data_gen(batchSize=32)
+
+    model.compile(loss='categorical_crossentropy',
+        optimizer='rmsprop',
+        metrics=['acc'])
+    
+    history = model.fit(
+        train_generator,
+        steps_per_epoch = _steps_per_epoch,
+        epochs = _epochs,
+        validation_data = validation_generator,
+        validation_steps = _validation_steps
+    )
+    model.save('char_recognition.h5')
+
+    display(history)
+
+def display(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+    
+
+def build(img_shape = (64, 64, 1), charset_size=100):
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=img_shape))
+    model.add(layers.MaxPool2D(2, 2))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPool2D(2, 2))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPool2D(2, 2))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPool2D(2, 2))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dense(charset_size, activation='softmax'))
+
+    return model
+
+
+model = build(charset_size=10)
+train(model)
